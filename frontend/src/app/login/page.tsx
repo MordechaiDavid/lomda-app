8e7542c860b5c75dc5c369d1cfb3cc4fa7e33d34
MordechaiDavid <mordechai.y.david@gmail.com>
2026-06-02 22:52:44 +0300

    fix config

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FormEvent, useState } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { apiService } from '../../lib/apiService';
 
